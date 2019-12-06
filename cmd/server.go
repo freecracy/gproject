4b5e464bb369f8b61fa26cf41ea9c8d422ebacd7
freecracy <12114073+freecracy@users.noreply.github.com>
2019-12-06 10:41:56 +0800

    init

--- cmd/server.go
+++ cmd/server.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,12 +17,12 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	_ "github.com/freecracy/gproject/statik"
 	"github.com/rakyll/statik/fs"
 	"github.com/spf13/cobra"
-	"log"
-	"net/http"
 )
 
 // serverCmd represents the server command
